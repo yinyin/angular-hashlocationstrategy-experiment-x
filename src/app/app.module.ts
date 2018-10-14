541d4f141dd3778afcc319d32a5a642f528e596b
Yinyin L <yinyinl@gmail.com>
2018-10-14 17:34:57 +0800

    add two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
+import {ApplesComponent} from './apples/apples.component';
+import {BicycleComponent} from './bicycle/bicycle.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ApplesComponent, BicycleComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bicycle/bicycle.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-bicycle',
+  templateUrl: './bicycle.component.html',
+  styleUrls: ['./bicycle.component.scss']
+})
+export class BicycleComponent {
+  constructor() {}
+}
