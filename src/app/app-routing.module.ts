8c15f71f2cff2bf266db6ca8cbbbe78544b35aa9
Yinyin L <yinyinl@gmail.com>
2018-10-14 17:45:17 +0800

    add routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {ApplesComponent} from './apples/apples.component';
+import {BicycleComponent} from './bicycle/bicycle.component';
 
-@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
+const routes: Routes = [
+  {path: 'fruit/some-apples', component: ApplesComponent},
+  {path: 'transport/bicycle', component: BicycleComponent},
+  {path: '', redirectTo: '/fruit/some-apples', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
+  exports: [RouterModule]
+})
 export class AppRoutingModule {
 }
