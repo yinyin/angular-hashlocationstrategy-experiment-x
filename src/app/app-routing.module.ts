3612df4078704b43a5dcccbf5d7449aedc8c71c0
Yinyin L <yinyinl@gmail.com>
2019-01-10 02:07:03 +0800

    implements fragment prefix support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
 @Injectable()
 export class ProposeHashLocationStrategy extends HashLocationStrategy {
   private urlBaseHref: string;
+  private urlFragmentPrefix: string;
+  private urlHashFragmentPrefix: string;
+
+  private sysPlatformLocation: PlatformLocation;
 
   constructor(
       _platformLocation: PlatformLocation,
@@ -25,18 +29,41 @@ export class ProposeHashLocationStrategy extends HashLocationStrategy {
       _baseHref = undefined;
     }
     _baseHref = _baseHref || _platformLocation.pathname;
-    this.urlBaseHref = _baseHref;
+    const hashSignAt = _baseHref.indexOf('#');
+    if (hashSignAt >= 0) {
+      this.urlBaseHref = _baseHref.substring(0, hashSignAt);
+      this.urlFragmentPrefix = _baseHref.substring(hashSignAt + 1);
+    } else {
+      this.urlBaseHref = _baseHref;
+      this.urlFragmentPrefix = '';
+    }
+    this.urlHashFragmentPrefix = '#' + this.urlFragmentPrefix;
+    this.sysPlatformLocation = _platformLocation;
   }
 
   getBaseHref(): string {
-    return this.urlBaseHref + '#';
+    return this.urlBaseHref + this.urlHashFragmentPrefix;
+  }
+
+  path(includeHash: boolean = false): string {
+    // the hash value is always prefixed with a `#`
+    // and if it is empty then it will stay empty
+    let path = this.sysPlatformLocation.hash;
+    if (path == null) {
+      path = this.urlHashFragmentPrefix;
+    }
+    if (path.startsWith(this.urlHashFragmentPrefix)) {
+      return path.substring(this.urlHashFragmentPrefix.length);
+    }
+    return path.length > 0 ? path.substring(1) : path;
   }
 
   prepareExternalUrl(internal: string): string {
     if (internal.length === 0) {
-      return this.urlBaseHref + '#/';
+      return this.urlBaseHref + this.urlHashFragmentPrefix + '/';
     }
-    const mark = internal.startsWith('/') ? '#' : '#/';
+    const mark = internal.startsWith('/') ? this.urlHashFragmentPrefix :
+                                            (this.urlHashFragmentPrefix + '/');
     return this.urlBaseHref + mark + internal;
   }
 }
