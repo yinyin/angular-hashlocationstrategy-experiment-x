5a7665f9c7b63d51db80600a6e9ad6283c04829d
Yinyin L <yinyinl@gmail.com>
2019-01-13 20:02:28 +0800

    add script to support rebasing after update master branch

---
+++ rebase-to-master.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ "master" != "$(git rev-parse --abbrev-ref HEAD)" ]; then
+	echo "ERR: must invoke at master branch."
+	exit 1
+fi
+
+if [ "" != "$(git diff --stat)" ]; then
+	echo "ERR: working copy must be clean."
+	exit 1
+fi
+
+do_rebase() {
+	REBASE_BRANCH=$1
+	REBASE_BASE=$2
+	echo "rebasing ${REBASE_BRANCH} to ${REBASE_BASE}."
+	git checkout "${REBASE_BRANCH}"
+	git rebase "${REBASE_BASE}" -i
+	echo "rebase complete."
+	sleep 2
+}
+
+BASEBRANCH="master"
+for NEXTBRANCH in "path-w-app-base-href" "hash-w-app-base-href" "hash-w-empty-app-base-href"; do
+	do_rebase "${NEXTBRANCH}" "${BASEBRANCH}"
+	BASEBRANCH=$NEXTBRANCH
+done
+
+BASEBRANCH="master"
+for NEXTBRANCH in "hash-wo-app-base-href" "proposehash-wo-app-base-href" "proposehash-w-app-base-href" "proposehash-w-empty-app-base-href"; do
+	do_rebase "${NEXTBRANCH}" "${BASEBRANCH}"
+	BASEBRANCH=$NEXTBRANCH
+done
